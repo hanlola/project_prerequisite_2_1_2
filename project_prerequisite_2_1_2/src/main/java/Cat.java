e340e7d434ecbe997824b631670449278a68bf27
hanlola <157224646+hanlola@users.noreply.github.com>
2024-02-21 00:38:58 +0900

    Add files via upload

---
+++ project_prerequisite_2_1_2/src/main/java/Cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+    Cat() {
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
